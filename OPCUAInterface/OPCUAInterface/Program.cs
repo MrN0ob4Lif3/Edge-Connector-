c8cf2c1e86943f9773d1cee93194741e12405077
Andrew Woon Yong Tian <1800984@sit.singaporetech.edu.sg>
2019-09-23 23:33:55 +0800

    Blank App to begin work.
    Tasks (on premise layer):
    
    1. Develop an IoT software service that will bridge between OPC UA and MQTT WS.
    2. Can be deployed in any OS platform (Windows, Linux)
    3. To be installed as a service (headless, run as system account, auto start on OS restarts)
    4. Configurable (datapoints, frequency, qos)
    5. Test

---
+++ OPCUAInterface/OPCUAInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OPCUAInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
