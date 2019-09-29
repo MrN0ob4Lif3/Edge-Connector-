c666a9f136c7a8db961d045444db0ae199d1d789
Andrew Woon Yong Tian <1800984@sit.singaporetech.edu.sg>
2019-09-30 01:20:54 +0800

    Updated timelog & dissertation

--- OPCUAInterface/OPCUAInterface/Program.cs
+++ OPCUAInterface/OPCUAInterface/Program.cs
@@ -7,10 +7,19 @@ namespace OPCUAInterface
 {
     class Program
     {
+        public static string test_hostname;
+
         static void Main(string[] args)
         {
+            Program.test_hostname = System.Net.Dns.GetHostName();
             Console.WriteLine("Hello World!");
+            Console.WriteLine(Program.test_hostname);
             Console.ReadKey();
         }
+
+        public void receiveMQTT()
+        { }
+        public void sendMQTT()
+        { }
     }
 }
