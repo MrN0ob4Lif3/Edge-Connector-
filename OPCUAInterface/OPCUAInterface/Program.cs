5da047ac67654a4ef097b67d3e2bae775314600f
Andrew Woon Yong Tian <1800984@sit.singaporetech.edu.sg>
2019-10-10 00:24:31 +0800

    Successul test of MQTTnet connection to local hosted MQTT broker (Mosquitto)

--- OPCUAInterface/OPCUAInterface/Program.cs
+++ OPCUAInterface/OPCUAInterface/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
-using System.Net.Mqtt;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 using Opc.Ua.Client;
 using Opc.Ua.Server;
 using Opc.Ua.Configuration;
-using System.ComponentModel.DataAnnotations;
 using MQTTnet;
+using MQTTnet.Client;
 using MQTTnet.Client.Options;
 using MQTTnet.Client.Receiving;
+using MQTTnet.Client.Subscribing;
+using MQTTnet.Server;
 
 
 namespace OPCUAInterface
@@ -25,79 +28,49 @@ namespace OPCUAInterface
             Console.WriteLine("Hello World!");
             Console.WriteLine(Program.test_hostname);
 
+            Task mainTask = MainAsync();
+            mainTask.Wait();
+
+        }
+
+        static async Task MainAsync()
+        {
+            await SendMQTT();
+            Console.ReadLine();
+        }
+
+        private static async Task SendMQTT()
+        {
             // Create a new MQTT client.
             var factory = new MqttFactory();
             var mqttClient = factory.CreateMqttClient();
+            Console.WriteLine("Starting MQTT Client");
 
             // Use WebSocket connection.
+            Console.Write("MQTT Broker IP: ");
+            string brokerIP = Console.ReadLine();
             var options = new MqttClientOptionsBuilder()
-                .WithWebSocketServer("broker.hivemq.com:8000/mqtt")
+                .WithTcpServer(brokerIP)
+                .Build();
+            Console.WriteLine("Connecting to " + brokerIP + " via TCP");
+
+            await mqttClient.ConnectAsync(options);
+        
+            // Message options
+            Console.Write("Topic: ");
+            string mqttTopic = Console.ReadLine();
+            Console.Write("Message: ");
+            string mqttMessage = Console.ReadLine();
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic(mqttTopic)
+                .WithPayload(mqttMessage)
+                .WithExactlyOnceQoS()
+                .WithRetainFlag()
                 .Build();
 
-            mqttClient.ConnectAsync(options, CancellationToken.None); // Since 3.0.5 with CancellationToken
-
-            //Topics for receipt and transmission
-            string rcvTopic = "eebus/daenet/command";
-            string sendTopic = "eebus/daenet/telemetry";
-
-            //Subscribes to the specified topic.
-            mqttClient.SubscribeAsync(rcvTopic, MqttQualityOfService.ExactlyOnce);
-
-            //Console to allow string messages to be typed and sent.
-            Task.Run(() =>
-            {
-                while (true)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-
-                    Console.WriteLine("Enter the text to send.");
-
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-
-                    var line = System.Console.ReadLine();
-
-                    var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(line));
-
-                    mqttClient.PublishAsync(new MqttApplicationMessage(sendTopic, data), MqttQualityOfService.ExactlyOnce).Wait();
-
-                    Console.WriteLine("Message sent");
-                }
-            });
-
-            //Subscribes to MQTT Server specified
-            mqttClient.MessageStream.Subscribe(msg =>
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-
-                Console.WriteLine(Encoding.UTF8.GetString(msg.Payload));
-
-                Console.ResetColor();
-            });
+            // Publishing
+            await mqttClient.PublishAsync(message);
+            Console.WriteLine("Message: " + mqttMessage + " published to Topic " + mqttTopic);
         }
-
-        public static async Task<IMqttClient> CreateMQTTClient(String serverAddress)
-        {
-            var serverIP = serverAddress;
-
-            var configuration = new MqttConfiguration
-            {
-                BufferSize = 128 * 1024,
-                Port = 55555,
-                KeepAliveSecs = 10,
-                WaitTimeoutSecs = 2,
-                MaximumQualityOfService = MqttQualityOfService.AtMostOnce,
-                AllowWildcardsInTopicFilters = true
-            };
-            var client = await MqttClient.CreateAsync(serverIP, configuration);
-            Console.WriteLine("Intializing MQTT Client to " + serverIP);
-            var sessionState = await client.ConnectAsync(new MqttClientCredentials("foo"), cleanSession: true);
-            Console.WriteLine("Connection Established");
-            return client;
-        }
-
-        public void SubscribeMQTT()
-        { }
-        public void PublishMQTT()
-        { }
     }
 }
